df2a70ffa849f1121a48adef586c5a9a69734742
PWang <501098462@qq.com>
2019-01-03 21:11:45 +0800

    1.versions.gradle 配置管理 2.增加一个shimmer 动画特效

--- app/src/main/java/com/pwang/wanandroid/feature/WelcomeActivity.java
+++ app/src/main/java/com/pwang/wanandroid/feature/WelcomeActivity.java
@@ -1,6 +1,12 @@
 package com.pwang.wanandroid.feature;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+
 import com.pwang.wanandroid.base.BaseActivity;
+import com.pwang.wanandroid.feature.home.MainActivity;
 
 /**
  * <pre>
@@ -12,4 +18,17 @@ import com.pwang.wanandroid.base.BaseActivity;
  * </pre>
  */
 public class WelcomeActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        gotoMainPage();
+    }
+
+    private void gotoMainPage(){
+        Intent intent = new Intent();
+        intent.setClass(WelcomeActivity.this, MainActivity.class);
+        ActivityCompat.startActivity(WelcomeActivity.this, intent, null);
+        finish();
+    }
 }
