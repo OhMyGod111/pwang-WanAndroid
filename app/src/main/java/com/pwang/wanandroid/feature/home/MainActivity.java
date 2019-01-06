5b90481f41d660b68a5a21b385ac7288639a4893
PWang <501098462@qq.com>
2019-01-06 18:47:46 +0800

    首页

--- app/src/main/java/com/pwang/wanandroid/feature/home/MainActivity.java
+++ app/src/main/java/com/pwang/wanandroid/feature/home/MainActivity.java
@@ -1,15 +1,54 @@
 package com.pwang.wanandroid.feature.home;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 import com.pwang.wanandroid.R;
 import com.pwang.wanandroid.base.BaseActivity;
 
-public class MainActivity extends BaseActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.fl_container)
+    FrameLayout flContainer;
+    @BindView(R.id.bottom_nav)
+    BottomNavigationView bottomNav;
+    @BindView(R.id.nav_menu)
+    NavigationView navMenu;
+    @BindView(R.id.drawerLayout)
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(MainActivity.this, drawerLayout,
+                toolbar,R.string.nav_open, R.string.nav_close);
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+
+        navMenu.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return false;
     }
 }
