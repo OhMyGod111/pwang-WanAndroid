3e5d4e8f601527335aa76e04c95d522fffa83e26
PWang <501098462@qq.com>
2019-03-24 01:45:38 +0800

    添加 RxBus组件用于通信

---
+++ app/src/main/java/com/pwang/wanandroid/common/RxBus.java
@@ -0,0 +1,13 @@
+package com.pwang.wanandroid.common;
+
+/**
+ * <pre>
+ *    @author :WangPan
+ *    @e-mail :501098462@qq.con
+ *    @time   :2019/03/24
+ *    @desc   :
+ *    @version:1.0
+ * </pre>
+ */
+public final class RxBus {
+}
