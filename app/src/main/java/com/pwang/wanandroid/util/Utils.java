dd5d8a173bf9e0bde1ade6bb51f25bd2a0037ae0
PWang <501098462@qq.com>
2019-04-20 21:54:09 +0800

    引入Activity管理工具类； 修复输入法引起的内存泄漏

--- app/src/main/java/com/pwang/wanandroid/util/Utils.java
+++ app/src/main/java/com/pwang/wanandroid/util/Utils.java
@@ -1,8 +1,20 @@
 package com.pwang.wanandroid.util;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.ActivityManager;
 import android.app.Application;
 import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * <pre>
@@ -16,22 +28,21 @@ import android.content.Context;
 public final class Utils {
 
     @SuppressLint("StaticFieldLeak")
-    private static volatile Context sInstance;
+    private static Application sInstance;
+    private static final ActivityLifecycleImpl ACTIVITY_LIFECYCLE = new ActivityLifecycleImpl();
 
     private Utils() {
+        throw new UnsupportedOperationException("u can't instantiate me...");
     }
 
     /**
      * 必须优先调用此初始化方法，才能使用相关的工具方法
      *
-     * @param context
+     * @param app
      */
-    public static void init(Context context) {
-        if (context instanceof Application) {
-            sInstance = Utils.requireNonNull(context, "context cannot be null");
-        }else {
-            throw new RuntimeException("Context is not an instance of Application ");
-        }
+    public static void init(Application app) {
+        sInstance = Utils.requireNonNull(app, "context cannot be null");
+        sInstance.registerActivityLifecycleCallbacks(ACTIVITY_LIFECYCLE);
     }
 
     /**
@@ -84,4 +95,157 @@ public final class Utils {
         return obj != null;
     }
 
+    /**
+     * @return
+     */
+    static List<Activity> getActivityList() {
+        return ACTIVITY_LIFECYCLE.mActivityList;
+    }
+
+    /**
+     * @return
+     */
+    static ActivityLifecycleImpl getActivityLifecycle() {
+        return ACTIVITY_LIFECYCLE;
+    }
+
+    /**
+     * 判断当前应用是否处在前台
+     * @return
+     */
+    static boolean isAppForeground() {
+        ActivityManager am = (ActivityManager) Utils.getAppContext().getSystemService(Context.ACTIVITY_SERVICE);
+        if (am == null) return false;
+        List<ActivityManager.RunningAppProcessInfo> info = am.getRunningAppProcesses();
+        if (info == null || info.size() == 0) return false;
+        for (ActivityManager.RunningAppProcessInfo aInfo : info) {
+            if (aInfo.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND) {
+                return aInfo.processName.equals(Utils.getAppContext().getPackageName());
+            }
+        }
+        return false;
+    }
+
+    static class ActivityLifecycleImpl implements Application.ActivityLifecycleCallbacks{
+        final LinkedList<Activity> mActivityList = new LinkedList<>();
+
+        @Override
+        public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+            addActivityToList(activity);
+        }
+
+        @Override
+        public void onActivityStarted(Activity activity) {
+
+        }
+
+        @Override
+        public void onActivityResumed(Activity activity) {
+            addActivityToList(activity);
+        }
+
+        @Override
+        public void onActivityPaused(Activity activity) {
+        }
+
+        @Override
+        public void onActivityStopped(Activity activity) {
+
+        }
+
+        @Override
+        public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+
+        }
+
+        @Override
+        public void onActivityDestroyed(Activity activity) {
+            mActivityList.remove(activity);
+            fixSoftInputLeaks(activity);
+        }
+
+        Activity getTopActivity() {
+            if (!mActivityList.isEmpty()) {
+                final Activity topActivity = mActivityList.getLast();
+                if (topActivity != null) {
+                    return topActivity;
+                }
+            }
+
+            Activity topActivityByReflect = getTopActivityByReflect();
+            if (topActivityByReflect != null) {
+                addActivityToList(topActivityByReflect);
+            }
+            return topActivityByReflect;
+        }
+
+        void addActivityToList(Activity activity){
+            if (mActivityList.contains(activity) && !mActivityList.getLast().equals(activity)) {
+                mActivityList.remove(activity);
+                mActivityList.addLast(activity);
+            } else {
+                mActivityList.addLast(activity);
+            }
+        }
+
+        /**
+         *  修复输入法导致的内存泄漏问题（SDK bug）
+         * @param activity 泄漏的Activity
+         */
+        private static void fixSoftInputLeaks(final Activity activity) {
+            if (activity == null) return;
+            InputMethodManager imm =
+                    (InputMethodManager) Utils.getAppContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+            if (imm == null) return;
+            String[] leakViews = new String[]{"mLastSrvView", "mCurRootView", "mServedView", "mNextServedView"};
+            for (String leakView : leakViews) {
+                try {
+                    Field leakViewField = InputMethodManager.class.getDeclaredField(leakView);
+                    if (leakViewField == null) continue;
+                    if (!leakViewField.isAccessible()) {
+                        leakViewField.setAccessible(true);
+                    }
+                    Object obj = leakViewField.get(imm);
+                    if (!(obj instanceof View)) continue;
+                    View view = (View) obj;
+                    if (view.getRootView() == activity.getWindow().getDecorView().getRootView()) {
+                        leakViewField.set(imm, null);
+                    }
+                } catch (Throwable ignore) { /**/ }
+            }
+        }
+
+        private Activity getTopActivityByReflect() {
+            try {
+                @SuppressLint("PrivateApi")
+                Class<?> activityThreadClass = Class.forName("android.app.ActivityThread");
+                Object currentActivityThreadMethod = activityThreadClass.getMethod("currentActivityThread").invoke(null);
+                Field mActivityListField = activityThreadClass.getDeclaredField("mActivityList");
+                mActivityListField.setAccessible(true);
+                Map activities = (Map) mActivityListField.get(currentActivityThreadMethod);
+                if (activities == null) return null;
+                for (Object activityRecord : activities.values()) {
+                    Class activityRecordClass = activityRecord.getClass();
+                    Field pausedField = activityRecordClass.getDeclaredField("paused");
+                    pausedField.setAccessible(true);
+                    if (!pausedField.getBoolean(activityRecord)) {
+                        Field activityField = activityRecordClass.getDeclaredField("activity");
+                        activityField.setAccessible(true);
+                        return (Activity) activityField.get(activityRecord);
+                    }
+                }
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            } catch (NoSuchFieldException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
 }
