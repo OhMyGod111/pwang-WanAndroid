3047d33213c8ecf1a21e6474dac86594fa07df89
PWang <501098462@qq.com>
2019-04-20 23:55:36 +0800

    引入CrashUtlis工具类，处理程序异常，日志收集等

---
+++ app/src/main/java/com/pwang/wanandroid/feature/browser/WebActivity.java
@@ -0,0 +1,23 @@
+package com.pwang.wanandroid.feature.browser;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.pwang.wanandroid.base.BaseActivity;
+
+/**
+ * <pre>
+ *    @author :WangPan
+ *    @e-mail :501098462@qq.con
+ *    @time   :2019/04/20
+ *    @desc   :
+ *    @version:1.0
+ * </pre>
+ */
+public class WebActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
